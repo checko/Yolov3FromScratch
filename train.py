ad1dc59529878b6a3f0988dc3f0acbc76f5a08e8
charles.chang <charles.chang@royaltek.com>
2025-03-25 14:58:16 +0800

    add train.py and necessary function in utils.py

---
+++ train.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+from yolov3 import YOLOv3
+from dataset import Dataset
+from utils import (
+    iou,
+    save_checkpoint,
+)
+from const import (
+    ANCHORS,
+    s,
+    device,
+    leanring_rate,  # Note: there's a typo in 'learning_rate'
+    batch_size,
+    epochs,
+    save_model
+)
+from dataset import train_transform
+
+# Defining YOLO loss class 
+class YOLOLoss(nn.Module): 
+	def __init__(self): 
+		super().__init__() 
+		self.mse = nn.MSELoss() 
+		self.bce = nn.BCEWithLogitsLoss() 
+		self.cross_entropy = nn.CrossEntropyLoss() 
+		self.sigmoid = nn.Sigmoid() 
+	
+	def forward(self, pred, target, anchors): 
+		# Identifying which cells in target have objects 
+		# and which have no objects 
+		obj = target[..., 0] == 1
+		no_obj = target[..., 0] == 0
+
+		# Calculating No object loss 
+		no_object_loss = self.bce( 
+			(pred[..., 0:1][no_obj]), (target[..., 0:1][no_obj]), 
+		) 
+
+		
+		# Reshaping anchors to match predictions 
+		anchors = anchors.reshape(1, 3, 1, 1, 2) 
+		# Box prediction confidence 
+		box_preds = torch.cat([self.sigmoid(pred[..., 1:3]), 
+							torch.exp(pred[..., 3:5]) * anchors 
+							],dim=-1) 
+		# Calculating intersection over union for prediction and target 
+		ious = iou(box_preds[obj], target[..., 1:5][obj]).detach() 
+		# Calculating Object loss 
+		object_loss = self.mse(self.sigmoid(pred[..., 0:1][obj]), 
+							ious * target[..., 0:1][obj]) 
+
+		
+		# Predicted box coordinates 
+		pred[..., 1:3] = self.sigmoid(pred[..., 1:3]) 
+		# Target box coordinates 
+		target[..., 3:5] = torch.log(1e-6 + target[..., 3:5] / anchors) 
+		# Calculating box coordinate loss 
+		box_loss = self.mse(pred[..., 1:5][obj], 
+							target[..., 1:5][obj]) 
+
+		
+		# Claculating class loss 
+		class_loss = self.cross_entropy((pred[..., 5:][obj]), 
+								target[..., 5][obj].long()) 
+
+		# Total loss 
+		return ( 
+			box_loss 
+			+ object_loss 
+			+ no_object_loss 
+			+ class_loss 
+		)
+
+
+# Define the train function to train the model 
+def training_loop(loader, model, optimizer, loss_fn, scaler, scaled_anchors): 
+	# Creating a progress bar 
+	progress_bar = tqdm(loader, leave=True) 
+
+	# Initializing a list to store the losses 
+	losses = [] 
+
+	# Iterating over the training data 
+	for _, (x, y) in enumerate(progress_bar): 
+		x = x.to(device) 
+		y0, y1, y2 = ( 
+			y[0].to(device), 
+			y[1].to(device), 
+			y[2].to(device), 
+		) 
+
+		with torch.cuda.amp.autocast(): 
+			# Getting the model predictions 
+			outputs = model(x) 
+			# Calculating the loss at each scale 
+			loss = ( 
+				loss_fn(outputs[0], y0, scaled_anchors[0]) 
+				+ loss_fn(outputs[1], y1, scaled_anchors[1]) 
+				+ loss_fn(outputs[2], y2, scaled_anchors[2]) 
+			) 
+
+		# Add the loss to the list 
+		losses.append(loss.item()) 
+
+		# Reset gradients 
+		optimizer.zero_grad() 
+
+		# Backpropagate the loss 
+		scaler.scale(loss).backward() 
+
+		# Optimization step 
+		scaler.step(optimizer) 
+
+		# Update the scaler for next iteration 
+		scaler.update() 
+
+		# update progress bar with loss 
+		mean_loss = sum(losses) / len(losses) 
+		progress_bar.set_postfix(loss=mean_loss)
+
+
+# Creating the model from YOLOv3 class 
+model = YOLOv3().to(device) 
+
+# Defining the optimizer 
+optimizer = optim.Adam(model.parameters(), lr = leanring_rate) 
+
+# Defining the loss function 
+loss_fn = YOLOLoss() 
+
+# Defining the scaler for mixed precision training 
+scaler = torch.cuda.amp.GradScaler() 
+
+# Defining the train dataset 
+train_dataset = Dataset( 
+	csv_file="./data/pascal voc/train.csv", 
+	image_dir="./data/pascal voc/images/", 
+	label_dir="./data/pascal voc/labels/", 
+	anchors=ANCHORS, 
+	transform=train_transform 
+) 
+
+# Defining the train data loader 
+train_loader = torch.utils.data.DataLoader( 
+	train_dataset, 
+	batch_size = batch_size, 
+	num_workers = 2, 
+	shuffle = True, 
+	pin_memory = True, 
+) 
+
+# Scaling the anchors 
+scaled_anchors = ( 
+	torch.tensor(ANCHORS) *
+	torch.tensor(s).unsqueeze(1).unsqueeze(1).repeat(1,3,2) 
+).to(device) 
+
+# Training the model 
+for e in range(1, epochs+1): 
+	print("Epoch:", e) 
+	training_loop(train_loader, model, optimizer, loss_fn, scaler, scaled_anchors) 
+
+	# Saving the model 
+	if save_model: 
+		save_checkpoint(model, optimizer, filename=f"checkpoint.pth.tar")
--- utils.py
+++ utils.py
@@ -195,3 +195,13 @@ def plot_image(image, boxes):
 
 	# Display the plot 
 	plt.show()
+
+# Function to save checkpoint 
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"): 
+	print("==> Saving checkpoint") 
+	checkpoint = { 
+		"state_dict": model.state_dict(), 
+		"optimizer": optimizer.state_dict(), 
+	} 
+	torch.save(checkpoint, filename)
+
